1bf230a0b6d2656681b1403f70d400ef8f61f96c
delver <dima.plugaturevchik@gmail.com>
2023-10-17 02:26:23 +0300

    Written the code for testing the work state of the custom stack collection

---
+++ CustomCollectionTestingApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using CustomCollectionLib;
+using CustomCollectionTestingApp;
+
+var stack = new CustomStack<string>();
+using var messageService = new StackMessageService<string>(stack);
+
+stack.Push("K");
+stack.Push("P");
+stack.Push("I");
+
+stack.Pop();
+
+Console.WriteLine("\nStack content:");
+foreach (var item in stack)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine($"Stack count: {stack.Count}");
+stack.Clear();
+Console.WriteLine($"Stack count: {stack.Count}");
